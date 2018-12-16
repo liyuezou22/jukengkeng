072d9234b4851e7fe9c9334bd34969b8efb6f0aa
lyz <liyuezou22@163.com>
2018-12-16 14:01:21 +0800

    ce

---
+++ jukengkengparent/jukengkeng-dao/src/main/java/com/jukengkeng/mapper/TbUserMapper.java
@@ -0,0 +1,30 @@
+package com.jukengkeng.mapper;
+
+import com.jukengkeng.pojo.TbUser;
+import com.jukengkeng.pojo.TbUserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbUserMapper {
+    int countByExample(TbUserExample example);
+
+    int deleteByExample(TbUserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbUser record);
+
+    int insertSelective(TbUser record);
+
+    List<TbUser> selectByExample(TbUserExample example);
+
+    TbUser selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbUser record, @Param("example") TbUserExample example);
+
+    int updateByExample(@Param("record") TbUser record, @Param("example") TbUserExample example);
+
+    int updateByPrimaryKeySelective(TbUser record);
+
+    int updateByPrimaryKey(TbUser record);
+}
